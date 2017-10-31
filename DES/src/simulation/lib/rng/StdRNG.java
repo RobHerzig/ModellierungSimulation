d5e4ed32a99d47c7e07c02105e891472db640987
robexitus <robexitus91@web.de>
2017-10-31 13:59:14 +0100

    Rest

--- DES/src/simulation/lib/rng/StdRNG.java
+++ DES/src/simulation/lib/rng/StdRNG.java
@@ -24,10 +24,7 @@ public class StdRNG extends RNG {
 	 */
 	@Override
 	public void setSeed(long seed) {
-		/*
-		 * TODO Problem 2.3.1 - setSeed
-		 * Update the seed of your random number generator rng (Hint: rng can be null @see RNG)
-		 */
+		rng = new Random(seed);
 	}
 
 	/**
@@ -35,11 +32,8 @@ public class StdRNG extends RNG {
 	 */
 	@Override
 	public double rnd() {
-		/*
-		 * TODO Problem 2.3.1 - rnd
-		 * Create a random value: 0 < value < 1
-		 */
 		double rv = 0;
+		rv = rng.nextDouble();
 		return rv;
 	}
 }
